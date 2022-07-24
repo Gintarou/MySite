3bd212c8699dee62b201e1ae5ff4359c359a42fb
小澤陸人 <ozawarikunin@rikuhitonoMacmini.local>
2022-07-24 16:08:51 +0900

    add: TODOコメント追加

--- src/utils/scrollObserver.tsx
+++ src/utils/scrollObserver.tsx
@@ -18,7 +18,7 @@ export const ScrollObserver: React.FC<Props> = ({ children }) => {
   const handleScroll = useCallback(() => {
     setScrollY(window.scrollY);
   }, []);
-
+  // TODO: スクロール時のhead再レンダリング問題修正
   useEffect(() => {
     document.addEventListener('scroll', handleScroll, { passive: true });
     return () => document.removeEventListener('scroll', handleScroll);
