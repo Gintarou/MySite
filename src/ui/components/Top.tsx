a9c8cf40f45e75e6bb02a16bbf0c0e10ccd8f11f
rikuto <rikuto.10141219@gmail.com>
2022-09-16 16:33:24 +0900

    edit: 文字を固定

--- src/ui/components/Top.tsx
+++ src/ui/components/Top.tsx
@@ -1,47 +1,31 @@
-import React, { useRef, useContext } from 'react';
+import React from 'react';
 
 import { css } from '@emotion/react';
 
 import BackgroundImage from '@/assets/mainBg.jpg';
-import { ScrollContext } from '@/utils/scrollObserver';
 import { theme } from '@/utils/theme';
 
-export const Top: React.FC = () => {
-  const refContainer = useRef<HTMLElement>(null);
-  const { scrollY } = useContext(ScrollContext);
+export const Top: React.FC = () => (
+  <section css={wrapper}>
+    <img src={BackgroundImage} alt="" css={background} />
+    <div css={headingWrapper}>
+      <h1 css={heading1}>Hello, world.</h1>
+    </div>
+  </section>
+);
 
-  let progress = 0;
-
-  const { current: elContainer } = refContainer;
-
-  if (elContainer) {
-    progress = Math.min(1, scrollY / elContainer.clientHeight);
-  }
-
-  return (
-    <section css={wrapper(progress)} ref={refContainer}>
-      <img src={BackgroundImage} alt="" css={background} />
-      <div css={headingWrapper(progress)}>
-        <h1 css={heading1}>Hello, world.</h1>
-      </div>
-    </section>
-  );
-};
-
-const wrapper = (progress: number) =>
-  css({
-    alignItems: 'center',
-    border: `20px solid ${theme.colors.white}`,
-    boxSizing: 'border-box',
-    display: 'flex',
-    justifyContent: 'center',
-    height: '60vh',
-    overflow: 'hidden',
-    position: 'sticky',
-    top: '0',
-    transform: `translateY(-${progress * 10}vh)`,
-    zIndex: '-1',
-  });
+const wrapper = css({
+  alignItems: 'center',
+  border: `20px solid ${theme.colors.white}`,
+  boxSizing: 'border-box',
+  display: 'flex',
+  justifyContent: 'center',
+  height: '60vh',
+  overflow: 'hidden',
+  position: 'sticky',
+  top: '0',
+  zIndex: '-1',
+});
 
 const background = css({
   height: '100%',
@@ -53,19 +37,21 @@ const background = css({
   width: '100%',
 });
 
-const headingWrapper = (progress: number) =>
-  css({
-    color: theme.colors.white,
-    fontSize: '40px',
-    fontWeight: 'bold',
-    textShadow: `4px 4px 3px ${theme.colors.blackAlpha40}`,
-    transform: `translateY(-${progress * 24}vh)`,
-    '@media (min-width: 600px)': {
-      fontSize: '60px',
-    },
-  });
+const headingWrapper = css({
+  color: theme.colors.white,
+  fontSize: '40px',
+  fontWeight: 'bold',
+  textShadow: `4px 4px 3px ${theme.colors.blackAlpha40}`,
+  '@media (min-width: 600px)': {
+    fontSize: '60px',
+  },
+});
 
 const heading1 = css({
   fontWeight: '900',
   lineHeight: '1.1em',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
 });
